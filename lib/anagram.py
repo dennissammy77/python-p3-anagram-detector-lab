4e4b72d80766841098dad22f8953c16127b703ea
dennissammy77 <dennissammy77@gmail.com>
2025-05-13 10:14:56 +0300

    anagrams

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,7 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = sorted(word)
+
+    def match(self, candidates):
+        return [w for w in candidates if sorted(w) == self.sorted_word]
